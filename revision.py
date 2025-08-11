b1abbb53414570f188153a250c775636a3fa39c9
Rakesh Zungaree <rakeshzungare14@live.in>
2025-08-11 23:30:25 +0530

    Your commit message

---
+++ revision.py
@@ -0,0 +1,81 @@
+#list1 = [1,2,3,4,5,6,7,8,2,2,3,3]
+
+
+#list1.extend([8,9,10])
+
+#list1.insert(0,21)
+
+#list1.remove(6)
+#print(list1)
+#print(list1.pop(0))
+
+#print(list1.count(2))
+
+#list1.sort()
+#print(list1)
+#list1.reverse()
+#print(list1)
+
+
+
+
+#Tuple
+
+
+#emp_tuple = (1,2,3,4,5,6,7,8,2,2,3,3)
+
+#print(emp_tuple.count(3))
+
+
+#print(emp_tuple.index(3))
+
+#emp_set.add(20)
+#emp_set = {1,2,3,4,5,6,7,8,2,2,3,3]
+
+
+#emp_set.update([30,40])
+#print(emp_set)
+
+
+#set1 = {1,2,3,4,5}
+#set2 ={2,3,5,6}
+
+
+#Union
+#set_result = set1 | set2
+#print(set_result)
+
+#set_result2 = set1 - set2
+#set_result3 = set1 & set2
+#print(set_result3)
+
+
+#emp1 = {1:"a",2:"c",3:"d",4:"f"}
+#print(emp1)
+#print(type(emp1))
+#print(emp1[3])
+
+#print(emp1.keys())
+#print(emp1.values())
+
+#emp1[6] ="h"
+#print(emp1)
+
+
+#emp1.pop(4)  # key has to be passed
+#emp1.popitem()  # last item removed
+#print(emp1)
+
+
+# Iterate dic & print emp name
+
+#for key , value in emp1.items():
+   # print(key, "=====>",value)
+
+
+
+
+
+
+
+
